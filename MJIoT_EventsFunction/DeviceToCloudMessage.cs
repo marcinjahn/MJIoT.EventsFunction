4535fce179d37817bcd1a7cbbd28eb22cbe3374d
Marcin Jahn <jahnmarcin@outlook.com>
2017-11-05 18:57:10 +0100

    Created DeviceToCloudMessage class

---
+++ MJIoT_EventsFunction/DeviceToCloudMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace MJIoT_EventsFunction
+{
+    public class DeviceToCloudMessage
+    {
+        public DeviceToCloudMessage()
+        {
+        }
+        public DeviceToCloudMessage(dynamic data)
+        {
+            DeviceId = data.DeviceId;
+            PropertyName = data.PropertyName;
+            Value = data.Value;
+            //Timestamp = data.Timestamp;
+        }
+
+        public int DeviceId { get; set; }
+        public string PropertyName { get; set; }
+        public string Value { get; set; }
+        //public string Timestamp { get; set; }
+    }
+}
