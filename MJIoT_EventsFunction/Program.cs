4bd6797512e649a2fb99b0ff5b9f41b17c59c76c
Marcin Jahn <jahnmarcin@outlook.com>
2017-11-05 20:32:52 +0100

    Added 2 methods: SaveValue and DeserializeMessage, which transforms string into instance of DeviceToCloudMessage; Changed type to library

--- MJIoT_EventsFunction/Program.cs
+++ MJIoT_EventsFunction/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MJIoT_DBModel;
+using Newtonsoft.Json;
 
 namespace MJIoT_EventsFunction
 {
-    class Program
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+
+    public class DeviceEventsHandler
     {
-        static void Main(string[] args)
+        public static DeviceToCloudMessage DeserializeMessage(string eventHubMessage)
         {
+            return JsonConvert.DeserializeObject<DeviceToCloudMessage>(eventHubMessage as string);
+        }
+
+        public static void SaveValue(DeviceToCloudMessage message)
+        {
+            using (var context = new MJIoTDBContext())
+            {
+                var deviceType = context.Devices
+                    .Where(n => n.Id == message.DeviceId)
+                    .Select(n => n.DeviceType)
+                    .FirstOrDefault();
+                if (deviceType == null)
+                {
+                    throw new NullReferenceException("Device (" + message.DeviceId + ") does not exist. Procedure aborted.");
+                }
+
+                var propertyType = context.PropertyTypes
+                    .Include("DeviceType")
+                    .Where(n => n.Name == message.PropertyName && n.DeviceType.Id == deviceType.Id)
+                    .FirstOrDefault();
+                if (propertyType == null)
+                {
+                    throw new NullReferenceException("Property type (" + message.PropertyName + ") does not exist. Procedure aborted.");
+                }
+
+                var property = context.DeviceProperties
+                    .Include("PropertyType").Include("Device")
+                    .Where(n => n.Device.Id == message.DeviceId && n.PropertyType.Id == propertyType.Id)
+                    .FirstOrDefault();
+
+                if (property == null)
+                {
+                    throw new NullReferenceException("Property (" + message.PropertyName + ") of device (" + message.DeviceId + ") does not exist. Procedure aborted.");
+                }
+
+                property.Value = message.Value;
+
+                context.SaveChanges();
+            }
         }
     }
 }
